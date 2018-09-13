53bcf6df27a3d19d092e9e11da414e9656e2ff10
Jason Walton <jwalton@benbria.ca>
2018-09-12 22:52:11 -0400

    test: Add comment to test [skip ci]

--- test/importTest.js
+++ test/importTest.js
@@ -2,7 +2,7 @@ import { expect } from 'chai';
 import amqp from '../src';
 
 describe('import test', function() {
-    it('should let you import as default', function() {
+    it('should let you import as default (#51)', function() {
         expect(amqp).to.exist;
         expect(amqp.connect).to.exist;
     });
