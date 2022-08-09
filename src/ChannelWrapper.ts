aaeae1e3e29ce5ef9598e6e8be1f377d9559ee2e
Mathias Lundell <lundell.mathias@gmail.com>
2022-08-09 14:39:29 +0200

    Revert "fix: import of promise breaker"
    This reverts commit d87388550d85e8326f6d91b6f86819809e1401ae.

--- src/ChannelWrapper.ts
+++ src/ChannelWrapper.ts
@@ -2,7 +2,7 @@ import type * as amqplib from 'amqplib';
 import { Options } from 'amqplib';
 import crypto from 'crypto';
 import { EventEmitter } from 'events';
-import * as pb from 'promise-breaker';
+import pb from 'promise-breaker';
 import { promisify } from 'util';
 import { IAmqpConnectionManager } from './AmqpConnectionManager.js';
 
